9c8310383115f85fb535af6adb38cef880984de5
Viktor <35453587+LifeGoal@users.noreply.github.com>
2021-03-15 19:26:07 +0100

    Trash code

--- ui/script.js
+++ ui/script.js
@@ -27,21 +27,13 @@ const CameraApp = new Vue({
 document.onreadystatechange = () => {
 	if (document.readyState === "complete") {
 		window.addEventListener('message', function (event) {
-
 			if (event.data.type == "enablecam") {
-
 				CameraApp.OpenCameras(event.data.box, event.data.label);
-
 			} else if (event.data.type == "disablecam") {
-
 				CameraApp.CloseCameras();
-
 			} else if (event.data.type == "updatecam") {
-
 				CameraApp.UpdateCameraLabel(event.data.label);
-
 			}
-
 		});
 	};
 };
